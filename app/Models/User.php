93dbaddbe4452d9c8a2f71666681de15be16c397
Ruswan <sukabumikode@gmail.com>
2023-07-23 05:58:53 +0700

    add trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,12 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 /**
  * Class User
@@ -47,7 +48,7 @@ use Laravel\Sanctum\HasApiTokens;
  */
 class User extends Authenticatable
 {
-	use HasApiTokens, HasFactory, Notifiable;
+	use HasApiTokens, HasFactory, Notifiable, HasRoles;
 	
 	protected $table = 'users';
 	
